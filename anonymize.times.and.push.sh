bb52ad871d3dcf156fe668e3ae91530ee918a52a
John Q Developer <202678382+JohnQDeveloper@users.noreply.github.com>
2025-04-12 00:00:00 +0000

    test3

--- anonymize.times.and.push.sh
+++ anonymize.times.and.push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export FILTER_BRANCH_SQUELCH_WARNING=1
 
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
   author_ts="$(git show -q --format="%at" "$GIT_COMMIT")"
   committer_ts="$(git show -q --format="%ct" "$GIT_COMMIT")"
   GIT_AUTHOR_DATE="$(date -d "@$author_ts" +"%Y-%m-%dT00:00:00 +0000")"
